dbec63b582dac9c5d5a962e2d153966702fe3356
khacngocPtit <khacngocit@gmail.com>
2019-09-13 00:35:30 +0700

    up

---
+++ js/main.js
@@ -0,0 +1,31 @@
+const instance = new TypeIt('#my-friend-name', {
+    strings: ['Lê Phương', 'Học Viện Ngân Hàng'],
+    speed: 300,
+    autoStart: false,
+
+}).go();
+
+
+
+$(document).ready(function() {
+    $('.customer-logos').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 1500,
+        arrows: false,
+        dots: false,
+        pauseOnHover: false,
+        responsive: [{
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 4
+            }
+        }, {
+            breakpoint: 520,
+            settings: {
+                slidesToShow: 3
+            }
+        }]
+    });
+});
